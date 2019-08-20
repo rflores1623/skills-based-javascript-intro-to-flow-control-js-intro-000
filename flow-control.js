451614b7215e43c768150f660b9aa53310cd6dd9
Roberta Flores <r.flores.1227@gmail.com>
2019-08-20 07:45:35 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -39,3 +39,32 @@ function ternaryTeenager(age) {
 	return (age >= 13 && age <= 19)?"You are a teenager":"You are not a teenager";
 }
 ternaryTeenager();
+
+
+function switchAge(age) {
+	switch(age) {
+		case 13:
+		return "You are a teenager";
+		break;
+		case 14:
+		return "You are a teeenager";
+		break;
+		case 15:
+		return "You are a teeenager";
+		break;
+		case 16:
+		return "You are a teeenager";
+		break;
+		case 17:
+		return "You are a teeenager";
+		break;
+		case 18:
+		return "You are a teeenager";
+		break;
+		case 19:
+		return "You are a teeenager";
+		break;
+		default:
+		return "You have an age";
+	}
+}

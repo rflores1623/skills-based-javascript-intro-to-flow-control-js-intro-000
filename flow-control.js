f5f10dbc9ff7781206163875b337f7287c6867d5
Roberta Flores <r.flores.1227@gmail.com>
2019-08-20 06:42:40 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -24,8 +24,8 @@ teenager();
 
 function ageChecker(age) {
 	if ( age >= 13 && <= 19) {
-		return "You are a teenager";
-	} else if ( age >= 13) {
+		return "You are a teenager!";
+	} else if ( age >= 13 ) {
 		return "You are a kid!";
 	} else {
 		return "You are a grownup";

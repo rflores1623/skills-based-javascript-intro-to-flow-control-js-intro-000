c45cbaec5a28ea115f405dcd06800b4bc80edca7
Roberta Flores <r.flores.1227@gmail.com>
2019-08-20 05:30:40 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,14 @@
 function basicTeenager(age) {
-
+	if (age >= 13 && age <= 19) {
+		return 'You are a teenager!';
+	}
 }
+basicTeenager;
 
 function teenager(age) {
-
-}
-
-function ageChecker(age) {
-
-}
-
-function ternaryTeenager(age) {
-
-}
-
-function switchAge(age) {
-
-}
+	if (age >= 13 && age <= 19) {
+		console.log('You are a teenager!')
+	} else {
+		console.log('You are not a teenager')
+	}
+teenager;

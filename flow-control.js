af6df611aba92604bfa0cf1296b6667711d094ba
Roberta Flores <r.flores.1227@gmail.com>
2019-08-20 05:52:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,6 @@
 function basicTeenager(age) {
-	if (age >= 13 && age =< 19) {
+	if ( age <= 13 && >= 19) {
+		return "You are a teenager!"
+	}
+	return basicTeenager(age);
 }
-	return "You are a teeneager!";
-}
-basicTeenagere();
